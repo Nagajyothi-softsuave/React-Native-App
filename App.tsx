c64163ed935b197b79e72e5b233f8eb8aa84db6b
Abdul-SS <abdul.a@softsuave.com>
2025-05-17 19:29:39 +0530

    Updated changes

--- App.tsx
+++ App.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
 import {SheetProvider} from 'react-native-actions-sheet';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Sheet from './src/Sheet';
 
-const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
-
   return (
     <SafeAreaProvider>
     <SheetProvider>
-    <NavigationContainer>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}  options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+     <Sheet />
     </SheetProvider>
     </SafeAreaProvider>
   );
--- src/Sheet.js
+++ src/Sheet.js
@@ -1,10 +1,10 @@
 import React, {useRef} from 'react';
 import {Button, Text, TouchableOpacity, View} from 'react-native';
-import {styles} from './styles';
 import ActionSheet from 'react-native-actions-sheet';
-import SheetHeader from '../SheetHeader';
+import SheetHeader from './SheetHeader';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-export default function HomeScreen() {
+export default function Sheet() {
   const actionSheetRef = useRef(null);
 
   const openSheet = () => {
@@ -32,3 +32,36 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+export const styles = {
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#ffffff',
+    padding: hp(10),
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button: {
+    borderRadius: 10,
+    backgroundColor: 'blue',
+    padding:10,
+    margin:10
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize:16,
+    fontWeight:'bold'
+  },
+  sheetContent: {
+    padding: 20,
+    alignItems: 'center',
+  },
+  sheetText: {
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+    marginTop:15,
+    fontFamily:'Times New Roman'
+  },
+};
